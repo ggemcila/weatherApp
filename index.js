0f12f6a738348934b40397388ffd41b5f2f67122
Gemcila <ggemcila@gmail.com>
2021-07-02 14:14:06 +0100

    #5 index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const express = require("express");
+const hbs = require("express-handlebars");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('hbs', hbs({
+    defaultLayout:'layout',
+    extname:'.hbs',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir:path.join(__dirname, 'views', 'partials')
+}));
+
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', '.hbs');
+
+app.listen(3000, () =>{
+    console.log('listening on port 3000');
+});
+
  
